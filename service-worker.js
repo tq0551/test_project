1a87857e87f2bcdbac406e122d595464b17ecf30
tq0551 <tq0551@hotmail.com>
2017-06-05 09:47:38 +0800

    3
    3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-// service-worker.js
+const isSupportServiceWorker = () => SERVICE_WORKER_API in navigator;
+
 const _self = this;
 
 console.log('In service worker.');
@@ -10,3 +11,13 @@ _self.addEventListener('install', function () {
 _self.addEventListener('activate', function () {
     console.log('Activated');
 });
+
+if (isSupportServiceWorker()) {
+    navigator
+        .serviceWorker
+        .register(SERVICE_WORKER_FILE_PATH)
+        .then(() => console.log('Load service worker Success.'))
+        .catch(() => console.error('Load service worker fail'));
+} else {
+    console.info('Browser not support Service Worker.');
+}
